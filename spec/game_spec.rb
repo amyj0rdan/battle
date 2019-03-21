a303350982f70928dcab193e02461949c944f65f
Amy Jordan <amy_jordan@live.co.uk>
2019-03-21 16:35:22 +0000

    refactored to make controllers skinnier

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,23 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+
   let(:player_1) { double :player }
   let(:player_2) { double :player }
+  subject(:game) { described_class.new(player_1, player_2) }
 
   it 'player 2 receives damage' do
     expect(player_2).to receive(:receive_damage)
     game.attack(player_2)
   end
+
+  it 'returns player 1' do
+    expect(game.player_1).to eq(player_1)
+  end
+
+  it 'returns player 2' do
+    expect(game.player_2).to eq(player_2)
+  end
+
 
 end
